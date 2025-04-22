47300d31f48471a1647baa7489f8d0dcd02468b2
Muhammad-Afnan-Siddiqui <afnan.siddiqui97@gmail.com>
2025-04-22 02:00:19 -0400

    Practice Bash Script no 1

---
+++ firstscript.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+echo "Welcom to bash script"
+echo
+
+echo "The uptime of the system is: "
+uptime
+
+echo "Memory Utilization: "
+free -m
+
+echo "Disk Utilization: "
+df -h 
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+
